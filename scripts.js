fca099e44a2fcf3801c9c8a6b5f6e5dfb47dd2b6
WagnerSkull <wagnerskull@hotmail.com>
2023-01-16 16:06:13 -0300

    Feedback and fixing bugs

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
+//Mapeando a imagem e o circulo 
 let imagem = document.querySelector(".starbucks")
     let circulo = document.querySelector(".circulo")
     
+    //Troca a imagem 
     function trocaImagem(endereco) {
         imagem.src = endereco
     }
-    
+    //Troca a cor de fundo do copo.
     function trocaCor(cor) {
         circulo.style.background = cor
     }
